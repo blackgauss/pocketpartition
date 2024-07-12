b97655fa1a64ac10a2c5ff0a4b4ee11d6fec0f02
blackgauss <81921810+blackgauss@users.noreply.github.com>
2024-07-11 23:18:29 -0700

    added tree walk

--- pocketpartition/random_numerical.py
+++ pocketpartition/random_numerical.py
@@ -17,3 +17,25 @@ def RandomNumericalSemigroupWithGenus(g):
         x = random.choice(mingens)
         s = s.remove_minimal_generator(x)
     return s
+
+def RandomTreeWalk(g):
+    """
+    Generates a random numerical semigroup with a given genus.
+        https://github.com/gap-packages/numericalsgps/blob/master/gap/random.gi
+    Parameters:
+    g (int): The genus of the numerical semigroup.
+
+    Returns:
+    NumericalSemigroup: The generated numerical semigroup.
+    """
+    s = NumericalSemigroup(generators={1})
+    for i in range(g):
+        mingens = s.minimal_generating_set()
+        frob = s.frobenius_number
+        effective_gens = [gen for gen in mingens if gen > frob]
+        if effective_gens:
+            x = random.choice(effective_gens)
+            s = s.remove_minimal_generator(x)
+        else:
+            return s
+    return s
