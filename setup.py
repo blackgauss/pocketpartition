6ab388f0b9b168577006ee6264ec81f1e8492027
blackgauss <81921810+blackgauss@users.noreply.github.com>
2024-07-14 13:58:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pocketpartition',
-    version='0.-1.0',
+    version='0.3.0-alpha',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     url='https://github.com/blackgauss/pocketpartition',
