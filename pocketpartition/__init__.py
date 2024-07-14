d7140761aabd5fd41e39e35243de04024a4989b4
blackgauss <81921810+blackgauss@users.noreply.github.com>
2024-07-13 23:34:53 -0700

    Update __init__.py

--- pocketpartition/__init__.py
+++ pocketpartition/__init__.py
@@ -1,4 +1,4 @@
 from .numerical import NumericalSet, NumericalSemigroup
 from .partition import Partition
 from .random_numerical import *
-from semigroup_tree import *
+from .semigroup_tree import *
