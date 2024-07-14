fb500fa345c5df4f1d837312226a3e97426eb0b6
blackgauss <81921810+blackgauss@users.noreply.github.com>
2024-07-14 13:00:19 -0700

    broken version

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pocketpartition',
     version='0.2.0',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(),  # No need to specify 'where' if everything is at the top level
     url='https://github.com/blackgauss/pocketpartition',
     license='MIT',
     author='Erik Imathiu-Jones',
