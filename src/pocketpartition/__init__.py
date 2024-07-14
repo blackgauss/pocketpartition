331dfd183ed238ef3efb602c429953003284ddfc
blackgauss <81921810+blackgauss@users.noreply.github.com>
2024-07-14 12:50:20 -0700

    fixed namespaces

--- src/pocketpartition/__init__.py
+++ src/pocketpartition/__init__.py
@@ -2,3 +2,5 @@ from .core.numerical import NumericalSet, NumericalSemigroup
 from .core.partition import Partition
 from .utils.random_numerical import *
 from .utils.semigroup_tree import *
+
+__all__ = ['NumericalSet', 'NumericalSemigroup', 'Partition']  # Add all other imported functions if needed
