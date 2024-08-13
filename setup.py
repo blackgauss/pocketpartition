9a1ca2bae7465ab1a2a4991b1cdc0f9f122eca49
blackgauss <81921810+blackgauss@users.noreply.github.com>
2024-08-13 11:36:04 -0700

    fixed visibility of functions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pocketpartition',
-    version='1.1.0-alpha',
+    version='1.1.1-alpha',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     url='https://github.com/blackgauss/pocketpartition',
--- src/pocketpartition/__init__.py
+++ src/pocketpartition/__init__.py
@@ -6,6 +6,9 @@ from .core.numerical_functions import (
     get_gap_poset,
     get_void_poset,
 )
+from .core.kunz import (
+    kunz_tuple
+)
 from .core.partition import Partition
 from .core.random_numerical import RandomNumericalSemigroupWithGenus
 
@@ -18,4 +21,5 @@ __all__ = [
     'get_partition',
     'get_gap_poset',
     'get_void_poset',
+    'kunz_tuple'
 ]
